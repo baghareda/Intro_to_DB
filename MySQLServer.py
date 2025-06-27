4e770378cf928d720efc03794d7df4d9171e6bc4
Reda Bagha <baghareda@gmail.com>
2025-06-28 00:11:29 +0100

    sql server

---
+++ MySQLServer.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="baghareda",
+    password="2002"
+)
+
+cursor = connection.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+print("Database 'alx_book_store' created successfully!")
+cursor.close()
+connection.close()
